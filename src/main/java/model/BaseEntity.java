0a4661385a9e4d7db1685761a8d6ecffc8a7d086
real-deepmind <real.deepmind@gmail.com>
2025-08-21 08:20:11 +0330

    create Order.class with constructor and getter and setter

--- src/main/java/model/BaseEntity.java
+++ src/main/java/model/BaseEntity.java
@@ -1,7 +1,15 @@
 package model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity <ID extends Serializable> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private ID id;
 }
--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,5 +1,43 @@
 package model;
 
-public class Order {
+import jakarta.persistence.Entity;
 
+import java.time.LocalDate;
+
+@Entity
+public class Order extends BaseEntity<Long> {
+    private String customerName;
+    private LocalDate orderDate;
+    private Double totalAmount;
+    public Order() {}
+
+    public Order(String customerName, LocalDate orderDate, Double totalAmount) {
+        this.customerName = customerName;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 }
